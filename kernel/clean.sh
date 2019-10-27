8d6423efd1969c0cf93647a398767966ae51672f
Cesar <cesar.maximo@gmail.com>
2019-10-27 01:14:21 +0000

    cleaning: more fixs ...

--- kernel/clean.sh
+++ kernel/clean.sh
@@ -2,15 +2,15 @@
 
 export CROSS_COMPILE="../toolchain/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-gnueabi-"
 export TARGET_PRODUCT=asus
-export MTK_ROOT_CUSTOM=../mediatek
+export MTK_ROOT_CUSTOM=./mediatek
 
 echo "**** START CLEANING ****"
 make mrproper
 make distclean
 
-rm -rf ../mediatek/config/out/
-rm -rf ../mediatek/custom/out/
-rm -rf ../out/
+rm -rf ./mediatek/config/out/
+rm -rf ./mediatek/custom/out/
+rm -rf ./out/
 rm -rf ../build_result
 rm -rf kernelFile
 
